e9f549fbfd285506844fdffdfc8cce9aa00ccee3
anilkumarpagilla123 <anilkumarpagilla1@gmail.com>
2022-03-15 22:32:57 +0530

    bitwise and logical  operators in javascript

---
+++ bitwiseoperators.js
@@ -0,0 +1,22 @@
+let x=5;
+let y=25;
+//bitwise and
+alert("Bitwise And :"+(x&y))
+//bitwise or
+alert("Bitwise or :"+(x|y))
+//bitwise not
+alert("Bitwise Not :"+(~y));
+//bitwise xor
+alert("Bitwise xor :"+(x^y))
+//left shift
+alert("left shift :"+(x<<1))
+//right shift
+alert("right shift :"+(x>>1))
+
+//logical operators
+//logical and
+alert("logical And :"+(x&&y))
+//logical and
+alert("logical And :"+(x||y))
+//logical and
+alert("logical And :"+(!x))
