82cf83d5d81dd2750ee59011a8cd3c7a16e37e9c
anilkumarpagilla123 <anilkumarpagilla1@gmail.com>
2022-03-15 22:30:52 +0530

    basic operators in javascript

---
+++ basicoperators.js
@@ -0,0 +1,26 @@
+let x=10;
+let y=30;
+let z=x+y;
+let text1 = "anil";
+let text2 = "kumar";
+//adding strings
+let text3 = text1 + " " + text2;
+//addition
+alert("Addition :"+z);
+//multiplication
+alert("Multiplication :"+(x*y))
+//substraction
+alert("Substraction :"+(x-y))
+//division
+document.write(" Division :"+(x/y)+"<br>")
+//exponential
+document.write("Power :"+(x**y)+"<br>")
+//modulo division
+document.write("Modulo division :"+(x%y)+"<br>")
+//increment
+alert("Increment :"+(x++))
+//decrement
+alert("Decrement :"+(y++))
+//adding strings
+alert(text3)
+
