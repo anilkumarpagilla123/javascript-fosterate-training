7da6b3294fb3fc26a47261904f54f98c8b56f1cd
anilkumarpagilla123 <anilkumarpagilla1@gmail.com>
2022-03-16 23:25:08 +0530

    Datatypes in javascript

---
+++ Datatypes.js
@@ -0,0 +1,15 @@
+let name="ram"; //string
+let x=15    //numbers
+let y=12.8
+
+const cars = ["Saab", "Volvo", "bmw"];  //arrays
+const person = {firstName:"anil", lastName:"kumar", age:50, eyeColor:"blue"};//object
+alert(typeof(x))//returns number
+alert(typeof(y))//returns number
+alert(typeof(person))//returns object
+alert(typeof(cars))
+let check=true;
+alert(typeof(check))//return boolean
+//undefined name
+let k;
+alert(typeof(k))
