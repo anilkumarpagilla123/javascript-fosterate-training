5d8e4491d7cdfacf838d457286d7d0292dd9743d
anilkumarpagilla123 <anilkumarpagilla1@gmail.com>
2022-03-20 00:28:51 +0530

    Adding & subtracting the dates in Date file

--- Date.js
+++ Date.js
@@ -18,3 +18,19 @@ console.log(k.toString()); //returns Sat Mar 19 2022 19:31:06 GMT+0530 (India St
 console.log(k.toUTCString());//returns Sat, 19 Mar 2022 14:00:27 GMT
 console.log(k.toDateString()); //returns Sat Mar 19 2022
 console.log(k.toISOString());//returns 2022-03-19T14:02:02.290Z
+
+//adding and substracting the dates
+var day1 = new Date("08/25/2020");
+var day2 = new Date("12/25/2021");
+
+var difference = day2.getTime()-day1.getTime();
+
+document.write("Days:"+difference+"<br>");//returns numbers of days
+
+var dt = new Date("December 30, 2017 11:20:25");
+dt.setDate( dt.getDate() - 10 );
+document.write(dt+"<br>")//substract 10 days from above date
+
+var dt = new Date("December 30, 2017 11:20:25");
+dt.setDate( dt.getDate() + 10 );
+document.write(dt+"<br>")//adding 10 to above date
