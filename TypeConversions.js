10a525eeb20050d9a272f2d315a9677495f4c2ec
anilkumarpagilla123 <anilkumarpagilla1@gmail.com>
2022-03-19 18:55:43 +0530

    type conversions

---
+++ TypeConversions.js
@@ -0,0 +1,22 @@
+//type conversions
+
+//boolean to string 
+let value = true;
+alert(typeof value); // boolean
+
+value = String(value); // now value is a string "true"
+alert(typeof value); // string
+
+//String to Number 
+let str="12345";
+alert(typeof str)
+let num=Number(str);
+alert(typeof num)
+
+//string to boolean
+alert(Boolean("hai"))//returns true
+alert(Boolean(""))//returns false
+
+//number to string 
+let x=24
+alert(typeof String(x));
